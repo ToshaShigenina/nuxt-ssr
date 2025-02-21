213cc5590424f8f4808925f79e5c5e2126e5efee
ToshaShigenina <Shigenina@gmail.com>
2025-02-21 20:44:42 +0500

    commit

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,16 @@
 import vuetify, { transformAssetUrls } from 'vite-plugin-vuetify'
 export default defineNuxtConfig({
+  ssr: false,
   app: {
+    head: {
+      meta: [
+        { charset: 'utf-8' },
+        { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      ],
+      link: [
+        { rel: 'icon', type: 'image/svg', href: '/favicon.svg' }
+      ]
+    },
     baseURL: '/nuxt-ssr/',
   },
   build: {
